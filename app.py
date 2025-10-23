df6f828a431160cf9500c52607c7618a8b076470
dhiaromdhane7-png <dhiaromdhane7@gmail.com>
2025-10-23 19:52:15 +0100

    Initial commit - Olympic Flask app2

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import pickle
 import numpy as np
 import pandas as pd
 from io import StringIO
+import tkinter as tk
+from tkinter import messagebox
+import sqlite3
 
 app = Flask(__name__)
 app.secret_key = "supersecretkey"
